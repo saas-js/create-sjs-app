bd371503fea9143858c8efa32e3027a83190cdf4
János Veres <janos@dot.industries>
2022-08-08 17:45:23 +0200

    chore: buildable cli app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,57 @@
 module.exports = {
+  $schema: "http://json.schemastore.org/eslintrc",
   root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    cli: "./apps/cli/tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/strict",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  env: {
+    node: true,
+    es6: true,
+  },
+  rules: {
+    "no-shadow": 1,
+    "import/no-named-as-default": 0,
+    "import/no-named-as-default-member": 0,
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "type",
+          "builtin",
+          "external",
+          "internal",
+          ["parent", "sibling", "index"],
+          "unknown",
+        ],
+        "newlines-between": "never",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        warnOnUnassignedImports: true,
+      },
+    ],
+  },
   settings: {
     next: {
       rootDir: ["apps/*/"],
     },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+        project: "./apps/cli/tsconfig.json",
+      },
+    },
   },
+  ignorePatterns: ["node_modules/", "build/", "dist/"],
 };
